f71ff44d1acbbfbbd65afdb6cec37aeb54f43a94
Oleg <egsov@mail.ru>
2022-01-14 17:20:14 +0500

    intereset distance support

--- src/rooms/BaseRoom.ts
+++ src/rooms/BaseRoom.ts
@@ -1,36 +1,39 @@
-import {WebSocket} from 'ws';
-import {BaseSystem, NumberPool} from 'ecs-threejs';
-import {ServerApp} from '../network/ServerApp';
-import {ExtWebSocket} from '../network/WsServer';
-import {DataHelper, protocol, utils, netUtils, MessagesPackerList} from '2d-client-network';
-import {BaseEntity, keyboardState} from '../entitys/BaseEntity';
-import {DataHelperPool} from '../network/DataHelperPool';
+import { WebSocket } from 'ws';
+import { BaseSystem, ItemInfo, NumberPool, SpatialHashingInterest } from 'ecs-threejs';
+import { ServerApp } from '../network/ServerApp';
+import { ExtWebSocket } from '../network/WsServer';
+import { DataHelper, protocol, utils, netUtils, MessagesPackerList } from '2d-client-network';
+import { BaseEntity, keyboardState } from '../entitys/BaseEntity';
+import { DataHelperPool } from '../network/DataHelperPool';
+import { mergeBuffer } from '2d-client-network/src/core/utils';
 
-interface ConnectionData{
-	idEntity:number;
-	idUser:number;
-	keyboard:keyboardState;
-	socket:ExtWebSocket;
+interface ConnectionData {
+	idEntity: number;
+	idUser: number;
+	keyboard: keyboardState;
+	socket: ExtWebSocket;
 }
 
 // type IdsMessage = (keyof typeof protocol.TypMessages);
 type IdsMessage = number;
 
-export class BaseRoom extends BaseSystem{
+export class BaseRoom extends BaseSystem {
 
-	public connectedUsers:{[k:number]:ConnectionData} = {};
-	protected startTime:number;
-	protected app:ServerApp;
-	protected dataHelperPool:DataHelperPool;
-	protected dataHelper:DataHelper;
-	protected entitys:{[k:number]:BaseEntity} = {};
-	protected dynamicEntitys:{[k:number]:BaseEntity} = {};
-	protected numPool:NumberPool = new NumberPool(65535);
-	protected timeLock:number = 500;
-	protected listPacker:MessagesPackerList;
+	public connectedUsers: { [k: number]: ConnectionData } = {};
+	protected startTime: number;
+	protected app: ServerApp;
+	protected dataHelperPool: DataHelperPool;
+	protected dataHelper: DataHelper;
+	protected entitys: { [k: number]: BaseEntity } = {};
+	protected dynamicEntitys: { [k: number]: BaseEntity } = {};
+	protected numPool: NumberPool = new NumberPool(65535);
+	protected timeLock: number = 500;
+	protected listPacker: MessagesPackerList;
+	protected interestActive: boolean = true;
+	protected interesetDistance = 1500;
+	private interestUpdate: SpatialHashingInterest;
 
-	constructor(app:ServerApp, listPacker:MessagesPackerList)
-	{
+	constructor(app: ServerApp, listPacker: MessagesPackerList) {
 		super();
 		this.startTime = this.now();
 		this.app = app;
@@ -38,15 +41,14 @@ export class BaseRoom extends BaseSystem{
 		this.dataHelperPool = app.dataHelperPool;
 		var wrap = this.dataHelperPool.get();
 		this.dataHelper = wrap.item;
+		this.interestUpdate = new SpatialHashingInterest(this.interesetDistance, app.config.worldWrap, app.config.worldSize);
 	}
 
-	now()
-	{
+	now() {
 		return Date.now();
 	}
 
-	getOffsetTime()
-	{
+	getOffsetTime() {
 		return this.now() - this.startTime;
 	}
 
@@ -55,8 +57,7 @@ export class BaseRoom extends BaseSystem{
 	// network
 	// -----------------------------------------------------------------------
 
-	packMessage(idMessage:IdsMessage, message:protocol.IMessage, view:DataHelper)
-	{
+	packMessage(idMessage: IdsMessage, message: protocol.IMessage, view: DataHelper) {
 		if (idMessage < 0)
 			return this.error("Сообщение не определено", idMessage, message);
 		var messagePacker = this.listPacker[idMessage];
@@ -66,8 +67,7 @@ export class BaseRoom extends BaseSystem{
 		return view;
 	}
 
-	makeMessage(idMessage:IdsMessage, message:protocol.IMessage)
-	{
+	makeMessage(idMessage: IdsMessage, message: protocol.IMessage) {
 		var wrap = this.dataHelperPool.get();
 		var view = wrap.item;
 		this.packMessage(idMessage, message, view);
@@ -76,80 +76,67 @@ export class BaseRoom extends BaseSystem{
 		return arr;
 	}
 
-	sendSocketBuffer(socket:ExtWebSocket, buffer:Uint8Array)
-	{
-		if (socket && socket.readyState === WebSocket.OPEN)
-		{
+	sendSocketBuffer(socket: ExtWebSocket, buffer: Uint8Array) {
+		if (socket && socket.readyState === WebSocket.OPEN) {
 			socket.send(buffer);
 		}
 	}
 
-	sendSocket(socket:ExtWebSocket, idMessage:IdsMessage, message:protocol.IMessage)
-	{
+	sendSocket(socket: ExtWebSocket, idMessage: IdsMessage, message: protocol.IMessage) {
 		var pack = this.makeMessage(idMessage, message);
 		this.sendSocketBuffer(socket, pack);
 	}
 
-	sendTo(user:ConnectionData, idMessage:IdsMessage, message:protocol.IMessage)
-	{
+	sendToConnection(user: ConnectionData, idMessage: IdsMessage, message: protocol.IMessage) {
 		if (user && user.socket)
 			this.sendSocket(user.socket, idMessage, message);
 	}
 
-	setToUid(idUser:number, idMessage:IdsMessage, message:protocol.IMessage)
-	{
+	setToUid(idUser: number, idMessage: IdsMessage, message: protocol.IMessage) {
 		var user = this.connectedUsers[idUser];
 		if (!user)
 			return this.warn("setToUid не найден юзер:", idUser, idMessage);
-		return this.sendTo(user, idMessage, message);
+		return this.sendToConnection(user, idMessage, message);
 	}
 
-	sendAll(idMessage:IdsMessage, message:protocol.IMessage, exceptSocket:ExtWebSocket|null = null)
-	{
+	sendAll(idMessage: IdsMessage, message: protocol.IMessage, exceptSocket: ExtWebSocket | null = null) {
 		var pack = this.makeMessage(idMessage, message);
-		for (var idUser in this.connectedUsers)
-		{
+		for (var idUser in this.connectedUsers) {
 			const user = this.connectedUsers[idUser];
-			if (user && user.socket && user.socket != exceptSocket && user.socket.readyState === WebSocket.OPEN)
-			{
+			if (user && user.socket && user.socket != exceptSocket && user.socket.readyState === WebSocket.OPEN) {
 				user.socket.send(pack);
 			}
 		}
 	}
 
-	sendAllBuffer(buffer:Uint8Array, exceptSocket:ExtWebSocket|null = null)
-	{
-		for (var idUser in this.connectedUsers)
-		{
+	sendAllBuffer(buffer: Uint8Array, exceptSocket: ExtWebSocket | null = null) {
+		for (var idUser in this.connectedUsers) {
 			const user = this.connectedUsers[idUser];
-			if (user && user.socket && user.socket != exceptSocket && user.socket.readyState === WebSocket.OPEN)
-			{
+			if (user && user.socket && user.socket != exceptSocket && user.socket.readyState === WebSocket.OPEN) {
 				user.socket.send(buffer);
 			}
 		}
 	}
 
-	sendFullBuffer()
-	{
+	sendFullBuffer() {
+		if (this.dataHelper.length == 0)
+			return;
 		const buffer = this.dataHelper.toArray();
 		this.sendAllBuffer(buffer);
 		this.dataHelper.startWriting();
 	}
 
-	addPack(idMessage:IdsMessage, message:protocol.IMessage)
-	{
+	addPack(idMessage: IdsMessage, message: protocol.IMessage) {
 		this.packMessage(idMessage, message, this.dataHelper);
 	}
 
-	addBuffer(buffer:Uint8Array)
-	{
+	addBuffer(buffer: Uint8Array) {
 		this.dataHelper.writeRawBytes(buffer);
 	}
 
-	InsertFirstPack(idMessage:IdsMessage, message:protocol.IMessage)
-	{
-		var curBuffer = utils.copyBuffer(this.dataHelper.toArray()); // просто .toArray() вернет новый массив, но с указателем на старые элементы
-		this.dataHelper.startWriting(); // соответственно при изменении его, будет меняться исходный, т.е. по факту не копия старого.
+	InsertFirstPack(idMessage: IdsMessage, message: protocol.IMessage) {
+		var curBuffer = this.dataHelper.toArray();
+		this.dataHelper.startWriting();
 		this.addPack(idMessage, message);
 		this.addBuffer(curBuffer);
 	}
@@ -157,26 +144,24 @@ export class BaseRoom extends BaseSystem{
 	// -----------------------------------------------------------------------
 	// Room methods
 	// -----------------------------------------------------------------------
-	
-	wrapPosition(entity:BaseEntity)
-	{
+
+	wrapPosition(entity: BaseEntity) {
 		const w = this.app.config.worldSize.x * 0.5;
 		const h = this.app.config.worldSize.y * 0.5;
 		entity.updateState();
 		if (entity.position.x >= w)
-			entity.position.x -= 2*w;
+			entity.position.x -= 2 * w;
 		else if (entity.position.x <= -w)
-			entity.position.x += 2*w;
+			entity.position.x += 2 * w;
 
 		if (entity.position.y >= h)
-			entity.position.y -= 2*h;
+			entity.position.y -= 2 * h;
 		else if (entity.position.y <= -h)
-			entity.position.y += 2*h;
+			entity.position.y += 2 * h;
 		entity.applyParams();
 	}
 
-	encodeEntityState(state:any, velocityType = 'uint8')
-	{
+	encodeEntityState(state: any, velocityType = 'uint8') {
 		if (state.angle !== undefined)
 			state.angle = netUtils.degToByte(state.angle);
 		if (state.position !== undefined)
@@ -191,45 +176,67 @@ export class BaseRoom extends BaseSystem{
 		return state;
 	}
 
-	encodeWorldState(state:any)
-	{
+	encodeWorldState(state: any) {
 		return this.encodeEntityState(state, 'uint8');
 	}
 
 	// апдейт каждые N мс
-	getWorldState()
-	{
+	getWorldState(idUser: number) {
 		var list = [];
-		for (var id in this.dynamicEntitys)
-		{
-			var e = this.dynamicEntitys[id];
-			if (!e.isSyncNetwork())
-				continue;
-
-			var state:protocol.IEntityInfo = {
-				id:Number(id),
-				position:e.getPosition(),
-				velocity:e.getVelocity(),
-				angle:e.getRotationDeg()
-			};
-			state = this.encodeWorldState(state);
-			//console.log("WorldState", info);
-			list.push(state);
+		// рассчет по пространственному хешу
+		if (idUser > -1) {
+			this.interestUpdate.updateFull(this.dynamicEntitys);
+			const conn = this.connectedUsers[idUser];
+			if (conn) {
+				const userEntity = this.entitys[conn.idEntity];
+				if (userEntity) {
+					var _visibleSet = this.interestUpdate.getVisibleList(userEntity.get2dPosition());
+					var visibleSet = _visibleSet as Set<BaseEntity>;
+					for (let entity of visibleSet) {
+						if (!entity.isSyncNetwork())
+							continue;
+						var state: protocol.IEntityInfo = {
+							id: Number(entity.idEntity),
+							position: entity.getPosition(),
+							velocity: entity.getVelocity(),
+							angle: entity.getRotationDeg()
+						};
+						state = this.encodeWorldState(state);
+						//console.log("WorldState", info);
+						list.push(state);
+					}
+				}
+			}
 		}
+		else {
+			for (var id in this.dynamicEntitys) {
+				var e = this.dynamicEntitys[id];
+				if (!e.isSyncNetwork())
+					continue;
+				var state: protocol.IEntityInfo = {
+					id: Number(id),
+					position: e.getPosition(),
+					velocity: e.getVelocity(),
+					angle: e.getRotationDeg()
+				};
+				state = this.encodeWorldState(state);
+				//console.log("WorldState", info);
+				list.push(state);
+			}
+		}
+		//console.log("SendTo:",idUser, 'count:', list.length);
 		return list;
 	}
 
 	// инфа при подключении
-	getWorldInfo()
-	{
+	getWorldInfo() {
 		var wrap = this.dataHelperPool.get();
 		var view = wrap.item;
 
-		var msgTimestamp:protocol.IScTimestamp = {offsetTime:this.getOffsetTime()};
+		var msgTimestamp: protocol.IScTimestamp = { offsetTime: this.getOffsetTime() };
 		this.packMessage(protocol.IdMessages.IScTimestamp, msgTimestamp, view);
 
-		for (var id in this.entitys)
-		{
+		for (var id in this.entitys) {
 			var e = this.entitys[id];
 			// todo опасно если будет угол не целым числом или позиция
 			var state = this.encodeEntityState(e.getState() as any, 'uint16');
@@ -241,13 +248,11 @@ export class BaseRoom extends BaseSystem{
 		return buffer;
 	}
 
-	getNewId()
-	{
+	getNewId() {
 		return this.numPool.get();
 	}
 
-	addEntity(entity:BaseEntity, isDynamic = false, id = -1)
-	{
+	addEntity(entity: BaseEntity, isDynamic = false, id = -1) {
 		if (id == -1)
 			var id = this.getNewId();
 		if (isDynamic)
@@ -267,42 +272,50 @@ export class BaseRoom extends BaseSystem{
 		return id;
 	}
 
-	getEntity(id:number)
-	{
+	getEntity(id: number) {
 		return this.entitys[id];
 	}
 
-	removeEntity(id:number)
-	{
+	removeEntity(id: number) {
 		this.numPool.put(id, this.timeLock)
 		if (this.entitys[id])
 			this.entitys[id].onRemove();
 		delete this.entitys[id];
 		delete this.dynamicEntitys[id];
 
-		var msg:protocol.IScRemoveE = {idEntity:id};
+		var msg: protocol.IScRemoveE = { idEntity: id };
 		this.addPack(protocol.IdMessages.IScRemoveE, msg);
 	}
 
 	// -----------------------------------------------------------------------
 	// Room events
 	// -----------------------------------------------------------------------
-	
+
 	// отправка накопившихся сообщений всем
-	onSocketUpdate()
-	{
-		var msgTimestamp:protocol.IScTimestamp = {offsetTime:this.getOffsetTime()};
-		this.InsertFirstPack(protocol.IdMessages.IScTimestamp, msgTimestamp);
+	onSocketUpdate() {
+		var msgTimestamp: protocol.IScTimestamp = { offsetTime: this.getOffsetTime() };
 
-		var msgWorldState:protocol.IScWorldStateUpdate = {list:this.getWorldState()};
-		this.addPack(protocol.IdMessages.IScWorldStateUpdate, msgWorldState);
-
+		if (this.interestActive) {
+			for (var idUser in this.connectedUsers) {
+				const connection = this.connectedUsers[idUser];
+				const msgWorldState: protocol.IScWorldStateUpdate = { list: this.getWorldState(Number(idUser)) };
+				var timePack = this.makeMessage(protocol.IdMessages.IScTimestamp, msgTimestamp);
+				var statePack = this.makeMessage(protocol.IdMessages.IScWorldStateUpdate, msgWorldState);
+				var buffer = mergeBuffer(timePack, statePack)
+				this.sendSocketBuffer(connection.socket, buffer);
+			}
+		}
+		else {
+			this.InsertFirstPack(protocol.IdMessages.IScTimestamp, msgTimestamp);
+			const msgWorldState: protocol.IScWorldStateUpdate = { list: this.getWorldState(-1) };
+			this.addPack(protocol.IdMessages.IScWorldStateUpdate, msgWorldState);
+		}
+		// а ведь в буффере могли быть какие-то данные, надо их отправить
 		this.sendFullBuffer();
 	}
 
 	// сообщение ввода
-	onMessageInput(socket:ExtWebSocket, message:protocol.ICsInput)
-	{
+	onMessageInput(socket: ExtWebSocket, message: protocol.ICsInput) {
 		const user = this.connectedUsers[socket.idUser];
 		if (!user)
 			return;
@@ -312,8 +325,7 @@ export class BaseRoom extends BaseSystem{
 	}
 
 	// сообщение позиции мышки/джостика
-	onMessageCursor(socket:ExtWebSocket, message:protocol.ICsMouseAngle)
-	{
+	onMessageCursor(socket: ExtWebSocket, message: protocol.ICsMouseAngle) {
 		const user = this.connectedUsers[socket.idUser];
 		if (!user)
 			return;
@@ -322,8 +334,7 @@ export class BaseRoom extends BaseSystem{
 	}
 
 	// переподключился
-	onReconnect(socket:ExtWebSocket)
-	{
+	onReconnect(socket: ExtWebSocket) {
 		const idUser = socket.idUser;
 		this.log("Переподключение idUser:", idUser);
 
@@ -334,24 +345,22 @@ export class BaseRoom extends BaseSystem{
 	}
 
 	// подключился, авторизован
-	onJoin(socket:ExtWebSocket, info = {})
-	{
+	onJoin(socket: ExtWebSocket, info = {}) {
 		const idUser = socket.idUser;
 		this.connectedUsers[idUser] = {
-			idEntity:0,
-			idUser:idUser,
-			socket:socket,
-			keyboard:{keys:{}, mouseAngle:0},
+			idEntity: -1,
+			idUser: idUser,
+			socket: socket,
+			keyboard: { keys: {}, mouseAngle: 0 },
 		};
 		// Юзеру - инфу о соедиении
-		var msg:protocol.IScInit = {serverStartTime:BigInt(this.startTime), offsetTime:this.getOffsetTime(), idUser:idUser, data:JSON.stringify(info)};
+		var msg: protocol.IScInit = { serverStartTime: BigInt(this.startTime), offsetTime: this.getOffsetTime(), idUser: idUser, data: JSON.stringify(info) };
 		this.sendSocket(socket, protocol.IdMessages.IScInit, msg);
 		return true;
 	}
 
 	// отключился
-	onLeave(socket:ExtWebSocket)
-	{
+	onLeave(socket: ExtWebSocket) {
 		if (this.connectedUsers[socket.idUser])
 			var id = this.connectedUsers[socket.idUser].idEntity;
 		else
@@ -359,40 +368,34 @@ export class BaseRoom extends BaseSystem{
 		delete this.connectedUsers[socket.idUser];
 		this.log("Отключился idUser/idEntity:", socket.idUser, id);
 
-		var msg:protocol.IScLeave = {idUser:socket.idUser, id:id};
+		var msg: protocol.IScLeave = { idUser: socket.idUser, id: id };
 		this.addPack(protocol.IdMessages.IScLeave, msg);
 	}
 
 	// покидание комнаты
-	onLeaveRoom(socket:ExtWebSocket)
-	{
+	onLeaveRoom(socket: ExtWebSocket) {
 		this.log("Покинул комнату idUser:", socket.idUser);
 	}
 
 	// обработка сообщения
-	onMessage(socket:ExtWebSocket, typ:number, srcMessage:protocol.IMessage)
-	{
-		if (typ == protocol.IdMessages.ICsInput)
-		{
+	onMessage(socket: ExtWebSocket, typ: number, srcMessage: protocol.IMessage) {
+		if (typ == protocol.IdMessages.ICsInput) {
 			this.onMessageInput(socket, srcMessage as protocol.ICsInput);
 		}
 
-		if (typ == protocol.IdMessages.ICsMouseAngle)
-		{
+		if (typ == protocol.IdMessages.ICsMouseAngle) {
 			this.onMessageCursor(socket, srcMessage as protocol.ICsMouseAngle);
 		}
 
-		if (typ == protocol.IdMessages.ICsPing)
-		{
+		if (typ == protocol.IdMessages.ICsPing) {
 			var message = srcMessage as protocol.ICsPing;
-			var msg:protocol.IScPong = {clientTime:message.clientTime, offsetTime:this.getOffsetTime()};
+			var msg: protocol.IScPong = { clientTime: message.clientTime, offsetTime: this.getOffsetTime() };
 			this.sendSocket(socket, protocol.IdMessages.IScPong, msg);
 		}
 	}
 
 	// тик обновления мира
-	onUpdate(deltaTime:number)
-	{
+	onUpdate(deltaTime: number) {
 		// важно зафиксировать время последнего пересчета и отправить его в рассчет интерполяции(если апдейт происходит в другом времени),
 		// т.к. если сокет отправит сообщение о текущем времени,
 		// то по факту позиция объектов будет старая, а времени будет больше, ведь обновление происходит именно в этот момент
